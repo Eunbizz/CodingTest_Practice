cbdecf232a3087e2a530e05656aeded8a48082fc
termidata <eb001020@naver.com>
2022-07-08 18:43:22 +0900

    Update 10816 숫자카드2-1.py

--- 정렬 알고리즘/10816 숫자카드2-1.py
+++ 정렬 알고리즘/10816 숫자카드2-1.py
@@ -1,4 +1,7 @@
 # 사전 풀이
+# card의 숫자들을 dict의 key, 목표를 value로 하여 사전을 만든다
+# target 숫자들을 key로 하여 value를 출력한다
+
 n = int(input())
 cards = list(map(int, input().split()))
 cards.sort()
@@ -12,6 +15,7 @@ for i in cards:
         dic[i] += 1
     else:
         dic[i] = 1
+        
 for i in range(m):
     if targets[i] in dic:
         print(dic[targets[i]], end = ' ')
