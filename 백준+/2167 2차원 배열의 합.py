6c1ca0e8a36025130dec20d5f76797a5f9331398
termidata <eb001020@naver.com>
2022-08-29 20:23:43 +0900

    Update 2167 2차원 배열의 합.py

--- 백준+/2167 2차원 배열의 합.py
+++ 백준+/2167 2차원 배열의 합.py
@@ -1,17 +1,16 @@
 n, m = map(int, input().split())
-arr = []
+a = []
 for _ in range(n):
-    arr.append(list(map(int, input().split())))
+    a.append(list(map(int, input().split())))
 
-memo = [[0] * (m+1) for _ in range(n+1)]
+psum = [[0 for _ in range(m+1)] for _ in range(n+1)]
 
 for i in range(1, n+1):
     for j in range(1, m+1):
-        memo[m][n]
+        psum[i][j] = a[i-1][j-1] + psum[i][j-1] + psum[i-1][j] - psum[i-1][j-1]
 
 
 k = int(input())
 for i in range(k):
     i, j, x, y = map(int, input().split())
-
-## 미완성
+    print(psum[x][y] - psum[x][j-1] - psum[i-1][y] + psum[i-1][j-1])
