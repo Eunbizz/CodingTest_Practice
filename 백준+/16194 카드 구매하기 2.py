743468e44496d9b6e3360e8c0fa6bd3057338be1
termidata <eb001020@naver.com>
2022-08-30 19:53:53 +0900

    22.08.30

--- 백준+/16194 카드 구매하기 2.py
+++ 백준+/16194 카드 구매하기 2.py
@@ -1,10 +1,10 @@
 n = int(input())
-c = [0]  # c의 인덱스를 맞추기 위해 필요
+c = [0]
 c += list(map(int, input().split()))
 dp = [0] * 1001
 
 for i in range(1, n+1):
     dp[i] = c[i]
     for j in range(1, i+1):
-        dp[i] = max(dp[i], dp[i-j]+c[j])
+        dp[i] = min(dp[i], dp[i-j] + c[j])
 print(dp[n])
