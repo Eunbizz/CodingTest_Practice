e1f89b0f813b0e6cb1dbd110da9c8e308a886d44
termidata <eb001020@naver.com>
2022-08-30 18:23:52 +0900

    22.08.30

---
+++ 백준+/9095 1,2,3 더하기.py
@@ -0,0 +1,15 @@
+t = int(input())
+
+def sums(n):
+    if n == 1:
+        return(1)
+    elif n == 2:
+        return(2)
+    elif n == 3:
+        return(4)
+    else:
+        return sums(n-1) + sums(n-2) + sums(n-3)
+
+for _ in range(t):
+    n = int(input())
+    print(sums(n))
