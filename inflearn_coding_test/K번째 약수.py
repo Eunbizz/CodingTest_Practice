9876532baee99324b83c8113a5bcf2de98a99c2f
termidata <eb001020@naver.com>
2022-12-01 13:45:57 +0900

    Update K번째 약수.py

--- inflearn_coding_test/K번째 약수.py
+++ inflearn_coding_test/K번째 약수.py
@@ -1,14 +1,14 @@
-import sys
-sys.stdin = open('input.txt', 'rt')
 
 N, K = map(int, input().split())
 n = []
-for i in range(1, N+1):1
+
+for i in range(1, N+1):
     if N % i == 0:
         n.append(i)
 
-if len(n) >= K:
-    print(n[K-1])
+    if len(n) == K:
+        print(n[K-1])
+        break
 else:
     print(-1)       
 
